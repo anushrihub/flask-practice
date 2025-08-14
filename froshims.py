ff791a1e95a26eed164d8b29b1d9777e7d34edcb
Anushri <sastikaranushri@gmail.com>
2025-08-13 22:24:47 -0400

    sqlalchemy connection with application

--- froshims.py
+++ froshims.py
@@ -1,15 +1,46 @@
-from flask import Flask, render_template, request
+# import click
+# from flask import Flask, render_template, request
+# from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)  
+# app = Flask(__name__)  
 
-SPORTS = ('BasketBall', 'Soccer', 'Ultimate Frisbee')
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///froshims.db"
 
-@app.route("/")
-def index():
-    return render_template("froshims_index.html")
+# db = SQLAlchemy(app)
 
-@app.route("/register", methods=["POST"])
-def register():
-    if not request.form.get("name") or not request.form.get("sport"):
-        return render_template("failure.html")
-    return render_template("success.html")
+# SPORTS = ('BasketBall', 'Soccer', 'Ultimate Frisbee')
+
+# class Registrant(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100), nullable=False)
+#     sport = db.Column(db.String(100), nullable=False)
+
+# with app.app_context():
+#     db.create_all()
+
+# @app.route("/")
+# def index():
+#     return render_template("froshims_index.html", sports=SPORTS)
+
+# @app.route("/register", methods=["POST"])
+# def register():
+#     name = request.form.get("name")
+#     sport = request.form.get("sport")
+
+#     if not name:
+#         return render_template("error.html",message="Please enter a name.")
+#     if not sport:
+#         return render_template("error.html",message="Please enter a sport.")
+#     elif sport not in SPORTS:
+#         return render_template("error.html",message="Please enter valid sport.")
+    
+#     registrant = Registrant(name=name, sport=sport)
+#     db.session.add(registrant)
+#     db.session.commit()
+
+#     return render_template("success.html")
+
+# @app.route("/registrants")
+# def registrants():
+#     all_registrants = Registrant.query.all()
+#     return render_template("registrants.html", registrants=all_registrants)
